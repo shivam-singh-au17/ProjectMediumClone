9457778d50005531d789c144a2ccd69435e23476
asutoshb <beheraasutosh132@gmail.com>
2021-09-29 01:11:00 +0530

    added some feature to ourstory

--- src/components/OurStory/OurStory.jsx
+++ src/components/OurStory/OurStory.jsx
@@ -2,7 +2,153 @@ import React from "react";
 import "./OurStory.css";
 
 const OurStory = () => {
-  return <div></div>;
+  return <div className="ourstory">
+            <div className="top">
+               Every idea needs a <span> Medium.</span>
+            </div>
+
+          <div className="logo-outer">
+            <div className="logo">
+              <div className="logo-left">
+                The best ideas can change who we are. Medium is where those ideas take shape, take off, and spark powerful conversations. We’re an open platform where over 100 million readers come to find insightful and dynamic thinking. Here, expert and undiscovered voices alike dive into the heart of any topic and bring new ideas to the surface. Our purpose is to spread these ideas and deepen understanding of the world.
+
+              <br /><br />We’re creating a new model for digital publishing. One that supports nuance, complexity, and vital storytelling without giving in to the incentives of advertising. It’s an environment that’s open to everyone but promotes substance and authenticity. And it’s where deeper connections forged between readers and writers can lead to discovery and growth. Together with millions of collaborators, we’re building a trusted and vibrant ecosystem fueled by important ideas and the people who think about them.
+              </div>
+              <div className="logo-right">
+                <img src="https://media3.giphy.com/media/6jZmOE1sT8vqZVMSwg/200.gif?cid=790b76118d01d208e0357026b186212daecce765bf546054&rid=200.gif&ct=g" />
+              </div>
+            </div>
+          </div>
+            
+
+
+            <div className="pink-box">
+                <div className="pink-box-title">
+                  A living network of <br/>curious minds.
+                </div>
+                <div className="pink-box-para">
+                  Anyone can write on Medium. Thought-leaders, journalists, experts, and individuals with unique<br/>
+                   perspectives share their thinking here. You’ll find pieces by independent writers from around the<br/>
+                    globe, stories we feature and leading authors, and smart takes on our own suite of blogs and<br/> publications.
+                </div>
+
+              <div className="pink-box-bottom">
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-01_1tri-col-item-01.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>The Atlantic</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/DevonPrice_1DevonPrice.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Devon Price</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-03_1tri-col-item-03.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>GEN</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-04_1tri-col-item-04.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Scott Galloway</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/NYT_1NYT.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>New York Times</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/KakiOkumura_1KakiOkumura.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Kaki Okimura</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-07_1tri-col-item-07.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>OneZero</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/CarvellWallace_1CarvellWallace.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Carvel Wallace</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-09_1tri-col-item-09.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Elemental</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner pink-last">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/SarahCottrell_1SarahCottrell.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Sarah Cottrell</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner pink-last">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/tri-col-item-11_1tri-col-item-11.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>Forge</div>
+                    </div>
+                  </div>
+
+                  <div className="pink-box-bottom-inner pink-last">
+                    <div className="pink-box-bottom-image">
+                      <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/Level_1Level.png" alt="asdf"/>
+                    </div>
+                    <div className="pink-box-bottom-name">
+                        <div>LEVEL</div>
+                    </div>
+                  </div>
+                  
+              </div>
+
+            </div>
+
+            
+
+          
+        </div>;
 };
 
 export { OurStory };
