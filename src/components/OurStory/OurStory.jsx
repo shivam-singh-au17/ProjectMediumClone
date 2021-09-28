6f43739463268dd8d57c1c3df2467e08e160bc02
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-28 18:18:00 +0530

    added write file ourStory file

---
+++ src/components/OurStory/OurStory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./OurStory.css";
+
+const OurStory = () => {
+  return <div></div>;
+};
+
+export { OurStory };
---
+++ src/components/Write/Write.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Write.css"
+
+const Write = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export {Write}
+
