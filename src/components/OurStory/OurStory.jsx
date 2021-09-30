287b297cdabcae720aa8484f0ffff11f8d93a138
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-30 16:05:01 +0530

    change file location backend and frontend file

--- src/components/OurStory/OurStory.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./OurStory.css";
-
-const OurStory = () => {
-  return <div>OurStory</div>;
-};
-
-export { OurStory };
--- src/components/Write/Write.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Write.css"
-
-const Write = () => {
-    return <div>Write</div>;
-}
-
-export {Write}
-
