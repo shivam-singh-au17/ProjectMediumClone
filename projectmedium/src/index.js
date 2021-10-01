acb6ceb9b6e337bcd8c18d0c9f58fd43c4a432b6
Shivam Singh <shivamsingh4458@gmail.com>
2021-10-01 19:14:56 +0530

    adding some pakages

--- projectmedium/src/index.js
+++ projectmedium/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
+import Theme from './Contexts/ThemeContexts';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Theme >
+        <App />
+      </Theme >
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
