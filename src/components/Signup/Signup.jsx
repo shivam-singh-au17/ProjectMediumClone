3c675797c9c6b8fa70941f2f4fb7fc0af92e8af0
asutoshb <beheraasutosh132@gmail.com>
2021-10-01 11:31:00 +0530

    completed signup functionality

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 import './Signup.css';
+import {Redirect} from 'react-router-dom'
 
 export const Signup = () => {
-    return (
+    const [isAuth, setIsAuth] = React.useState(true);
+
+    const responseGoogle = (response) => {
+        console.log(response);
+        var res = response.profileObj;
+        console.log(res);
+        setIsAuth(false)
+    }
+
+
+    return isAuth ? (
         <div>
             <div className="signup-box">
                 <div className="signup-title">
                     Welcome to Medium
                 </div>
+
+                
+
                 <div className="signup-google">
-                    <div className="signup-google-text">
+                    <div style={{backgroundColor: "white", shadow:"none", border: "white",
+                borderRadius: "50%", outline: "none", boxShadow: "none", borderRight: "1px solid white"}}>
+                    <GoogleLogin 
+                    clientId="137235037828-76vpvig8vdp5l62eldlcefs5spfcrhao.apps.googleusercontent.com" 
+                    buttonText="Continue with Google" 
+                    onSuccess={responseGoogle} 
+                    onFailure={responseGoogle} 
+                    buttonOutline="none">
+
+                    </GoogleLogin>
+                </div>
+                    {/* <div className="signup-google-text">
                         <div className="signup-google-image">
                             <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png" />
                         </div>
                         <div className="signup-google-content"><div>Continue with Google</div></div>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className="signup-twitter">
@@ -42,5 +68,7 @@ export const Signup = () => {
 
             </div>
         </div>
+    ) : (
+        <Redirect to='/home' />
     )
 }
